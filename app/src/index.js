236d90f35c0e489192558579e71f19333f827176
Max Medina <maxmedina05@gmail.com>
2020-05-01 13:14:27 +0200

    finish task 4

--- app/src/index.js
+++ app/src/index.js
@@ -1,63 +1,70 @@
-import Web3 from "web3";
-import starNotaryArtifact from "../../build/contracts/StarNotary.json";
+import Web3 from 'web3'
+import starNotaryArtifact from '../../build/contracts/StarNotary.json'
 
 const App = {
   web3: null,
   account: null,
   meta: null,
 
-  start: async function() {
-    const { web3 } = this;
+  start: async function () {
+    const { web3 } = this
 
     try {
       // get contract instance
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = starNotaryArtifact.networks[networkId];
+      const networkId = await web3.eth.net.getId()
+      const deployedNetwork = starNotaryArtifact.networks[networkId]
       this.meta = new web3.eth.Contract(
         starNotaryArtifact.abi,
         deployedNetwork.address,
-      );
+      )
 
       // get accounts
-      const accounts = await web3.eth.getAccounts();
-      this.account = accounts[0];
+      const accounts = await web3.eth.getAccounts()
+      this.account = accounts[0]
     } catch (error) {
-      console.error("Could not connect to contract or chain.");
+      console.error('Could not connect to contract or chain.')
     }
   },
 
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
+  setStatus: function (message) {
+    const status = document.getElementById('status')
+    status.innerHTML = message
   },
 
-  createStar: async function() {
-    const { createStar } = this.meta.methods;
-    const name = document.getElementById("starName").value;
-    const id = document.getElementById("starId").value;
-    await createStar(name, id).send({from: this.account});
-    App.setStatus("New Star Owner is " + this.account + ".");
+  createStar: async function () {
+    const name = document.getElementById('starName').value
+    const id = document.getElementById('starId').value
+    await this.meta.methods.createStar(name, id).send({ from: this.account })
+    App.setStatus('New Star Owner is ' + this.account + '.')
   },
 
   // Implement Task 4 Modify the front end of the DAPP
-  lookUp: async function (){
-    
-  }
+  lookUp: async function () {
+    const tokenId = document.getElementById('lookid').value
+    const starName = await this.meta.methods
+      .lookUptokenIdToStarInfo(tokenId)
+      .call()
 
-};
+    App.setStatus('Star with token Id has to following name: ' + starName + '.')
+  },
+}
 
-window.App = App;
+window.App = App
 
-window.addEventListener("load", async function() {
+window.addEventListener('load', async function () {
   if (window.ethereum) {
     // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum);
-    await window.ethereum.enable(); // get permission to access accounts
+    App.web3 = new Web3(window.ethereum)
+    await window.ethereum.enable() // get permission to access accounts
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
+    console.warn(
+      'No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live',
+    )
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+    App.web3 = new Web3(
+      new Web3.providers.HttpProvider('http://127.0.0.1:9545'),
+    )
   }
 
-  App.start();
-});
+  App.start()
+})
